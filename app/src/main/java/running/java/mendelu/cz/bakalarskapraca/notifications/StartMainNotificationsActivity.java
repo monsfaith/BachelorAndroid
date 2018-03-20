46e7bfb243ea0e793a8380ff846b0eac2ae69c57
MON\Monika <xcanigov@mendelu.cz>
2018-03-20 17:40:50 +0100

    upozornenia7.5

--- app/src/main/java/running/java/mendelu/cz/bakalarskapraca/notifications/StartMainNotificationsActivity.java
+++ app/src/main/java/running/java/mendelu/cz/bakalarskapraca/notifications/StartMainNotificationsActivity.java
@@ -159,8 +159,8 @@ public class StartMainNotificationsActivity extends AppCompatActivity {
 
     private long getCurrentDate(){
         Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.HOUR_OF_DAY,0);
-        cal.set(Calendar.MINUTE,0);
+        //cal.set(Calendar.HOUR_OF_DAY,0);
+        //cal.set(Calendar.MINUTE,0);
         cal.set(Calendar.SECOND,0);
         cal.set(Calendar.MILLISECOND,0);
         return cal.getTimeInMillis();
