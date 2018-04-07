e968e38850bd7a49bacad41aa1f4a74e9fd93e16
MON\Monika <xcanigov@mendelu.cz>
2018-04-07 17:03:49 +0200

    pomaly247

--- app/src/main/java/running/java/mendelu/cz/bakalarskapraca/CreateSubjectActivity.java
+++ app/src/main/java/running/java/mendelu/cz/bakalarskapraca/CreateSubjectActivity.java
@@ -1,8 +1,12 @@
 package running.java.mendelu.cz.bakalarskapraca;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,16 +17,21 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import eltos.simpledialogfragment.SimpleDialog;
+import eltos.simpledialogfragment.color.SimpleColorDialog;
 import running.java.mendelu.cz.bakalarskapraca.db.Subject;
 import running.java.mendelu.cz.bakalarskapraca.db.SubjectMainRepository;
 
-public class CreateSubjectActivity extends AppCompatActivity {
+public class CreateSubjectActivity extends AppCompatActivity implements SimpleDialog.OnDialogResultListener {
 
+    private static final String COLOR_DIALOG = "color";
     //private Button subjectCreateButton;
     private EditText subjectNameInput;
-    private EditText subjectShortcutInput;
+    private FloatingActionButton subjectShortcutInput;
     private SubjectMainRepository subjectMainRepository;
     public static final String SUB_ID = "subId";
+    private Context context;
+    private String subjectColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +39,32 @@ public class CreateSubjectActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_subject);
         //subjectCreateButton = (Button) findViewById(R.id.createSubBtn);
         subjectNameInput = (EditText) findViewById(R.id.subjectNameInput);
-        subjectShortcutInput = (EditText) findViewById(R.id.subjectShortcutInput);
+        subjectShortcutInput = (FloatingActionButton) findViewById(R.id.subjectShortcutInput);
         subjectMainRepository = new SubjectMainRepository(this);
+        context = this;
+        subjectColor = "";
+
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        subjectShortcutInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createDialog();
+            }
+        });
+
+
     }
+
+    private void createDialog(){
+        SimpleColorDialog.build()
+                .title("Vyber si rozlišovaciu farbu predmetu")
+                .colorPreset(Color.RED)
+                .allowCustom(false)
+                .show(this, COLOR_DIALOG);
+    }
+
 
 
     /*public void newSubjectCreation(View v){
@@ -54,16 +83,30 @@ public class CreateSubjectActivity extends AppCompatActivity {
 
     public void createSubject(MenuItem item) {
         String subjectName = subjectNameInput.getText().toString();
-        String subjectShortcut = subjectShortcutInput.getText().toString().toUpperCase();
-        Subject sub = new Subject(subjectName,subjectShortcut);
-        Long lastInsertedId = subjectMainRepository.insert(sub);
+        if (subjectName.trim().length() == 0){
+            subjectNameInput.setError("Nutné vyplniť");
+        }
+        //String subjectShortcut = subjectShortcutInput.getText().toString().toUpperCase();
+        if (subjectColor.trim().length() == 0){
+            Toast.makeText(this,"Nie je zvolená farba", Toast.LENGTH_SHORT).show();
+        } else {
+            Subject sub = new Subject(subjectName, subjectColor);
+            Long lastInsertedId = subjectMainRepository.insert(sub);
 
-        Toast.makeText(CreateSubjectActivity.this,"" + lastInsertedId + "id",Toast.LENGTH_LONG).show();
+            Toast.makeText(CreateSubjectActivity.this, "" + lastInsertedId + " id " + subjectColor, Toast.LENGTH_LONG).show();
 
-        Intent i = new Intent();
-        i.putExtra(SUB_ID, lastInsertedId);
-        setResult(RESULT_OK,i);
-        finish();
+            Intent i = new Intent();
+            i.putExtra(SUB_ID, lastInsertedId);
+            setResult(RESULT_OK, i);
+            finish();
+        }
+    }
+
+    public void onResume(){
+        super.onResume();
+        if (subjectColor.trim().length() != 0){
+            //subjectShortcutInput.setBackgroundColor();
+        }
     }
 
         @Override
@@ -82,5 +125,17 @@ public class CreateSubjectActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
             inflater.inflate(R.menu.menu_create_sub, menu);
         return true;
+    }
+
+    @Override
+    public boolean onResult(@NonNull String dialogTag, int which, @NonNull Bundle extras) {
+        if (which == BUTTON_POSITIVE && COLOR_DIALOG.equals(dialogTag)){
+            subjectColor = extras.getString(SimpleColorDialog.COLOR);
+            Toast.makeText(this, subjectColor + "", Toast.LENGTH_LONG).show();
+            return true;
+        } else {
+            subjectColor = "";
+            return false;
+        }
     }
 }
